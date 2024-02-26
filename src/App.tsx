605a210f88d60221e520f05a04a158b4553229c3
Lemon <yellowwhite@vivaldi.net>
2024-02-26 11:57:38 +0300

    Did responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import Nav  from './Nav/Nav.jsx'
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const Subject = "Reaching Out"
+  const [salue,setSalue] = useState<string>("")
 
   return (
     <main>
       <Nav />
-      <div className="flex min-h-screen items-center gap-[10%] px-10 border-orange-500 bg-white w-full">
-        <div className="order-1 w-1/2">
+      <div className="flex min-h-screen flex-col mt-6 sm:flex-row items-center sm:gap-[10%] px-10 border-orange-500 bg-white w-full">
+        <div className="sm:order-2 w-full order-1 sm:w-1/2">
           <img className='w-full h-auto' src="./Group 1.svg" alt="" />
         </div>
-        <div className="order-0 w-1/2 px-12">
-          <h1 className='font-bold text-5xl leading-normal mb-10 text-sky-900'>
+        <div className="sm:order-1 order-2 w-full sm:w-1/2 sm:px-12 px-4">
+          <h1 className='font-bold sm:text-5xl text-3xl leading-normal mb-10 text-sky-900'>
             Crafting with code to build the future
           </h1>
           <p className='font-medium texl-lg text-sky-900 mb-6 opacity-70 leading-loose'>
@@ -23,46 +26,46 @@ function App() {
 
         </div>
       </div>
-      <h2 className='text-xl text-sky-900 font-semibold capitalize mb-4 m-16 '>web Technogies Learned</h2>
+      <h2 className='text-xl text-sky-900 font-semibold capitalize  m-6 sm:m-16 '>web Technogies Learned</h2>
       <section className='w-full flex flex-wrap px-16 gap-5' id="tech">
-        <div className="border-orange-200 w-1/5 border-2  rounded-md bg-white">
-          <div className="p-7">
+        <div className="border-orange-200 w-[45%] sm:w-1/5 border-2  rounded-md bg-white">
+          <div className="sm:p-7 p-4">
             <img src="./icons8-html.svg" className='w-full mx-auto h-auto' alt="" />
           </div>
           <p className='text-lg text-center text-md  text-sky-950 opacity-70 font-medium '>HTML</p>
         </div>
-        <div className="border-orange-200 w-1/5 border-2  rounded-md bg-white">
-          <div className=" p-7">
+        <div className="border-orange-200 w-[45%] sm:w-1/5 border-2  rounded-md bg-white">
+          <div className="sm:p-7 p-6">
             <img src="./icons8-css.svg" className='w-full mx-auto h-auto' alt="" />
           </div>
           <p className='text-lg text-center text-md  text-sky-950 opacity-70 font-medium '>CSS </p>
         </div>
-        <div className="border-orange-200 w-1/5   border-2 rounded-md bg-white">
-          <div className=" p-7">
+        <div className="border-orange-200 w-[45%] sm:w-1/5   border-2 rounded-md bg-white">
+          <div className="sm:p-7 p-4">
             <img src="./icons8-javascript.svg" className='w-full mx-auto h-auto' alt="" />
           </div>
           <p className='text-lg text-center text-md  text-sky-950 opacity-70 font-medium '>Javascript</p>
         </div>
-        <div className="border-orange-200 w-1/5 border-2  rounded-md bg-white">
-          <div className="p-7">
+        <div className="border-orange-200 w-[45%] sm:w-1/5 border-2  rounded-md bg-white">
+          <div className="sm:p-7 p-4">
             <img src="./icons8-nextjs.svg" className='w-full mx-auto h-auto' alt="" />
           </div>
           <p className='text-lg text-center text-md  text-sky-950 opacity-70 font-medium '>NextJs</p>
         </div>
-        <div className="border-orange-200 w-1/5 border-2  rounded-md bg-white">
+        <div className="border-orange-200 w-[45%] sm:w-1/5 border-2  rounded-md bg-white">
           <div className="p-7">
             <img src="./icons8-tailwindcss.svg" className='w-full mx-auto h-auto' alt="" />
           </div>
           <p className='text-lg text-center text-md  text-sky-950 opacity-70 font-medium '>Tailwindcss</p>
         </div>
-        <div className="border-orange-200 w-1/5 border-2  rounded-md bg-white">
-          <div className="p-7">
+        <div className="border-orange-200 w-[45%] sm:w-1/5 border-2  rounded-md bg-white">
+          <div className="sm:p-7 p-4">
             <img src="./icons8-react-native.svg" className='w-full mx-auto h-auto' alt="" />
           </div>
           <p className='text-lg text-center text-md  text-sky-950 opacity-70 font-medium '>ReactJs</p>
         </div>
-        <div className="border-orange-200 w-1/5 border-2  rounded-md bg-white">
-          <div className=" p-7">
+        <div className="border-orange-200 w-[45%] sm:w-1/5 border-2  rounded-md bg-white">
+          <div className=" sm:p-7 p-4">
             <img src="./icons8-nodejs.svg" className='w-full mx-auto h-auto' alt="" />
           </div>
           <p className='text-lg text-center text-md  text-sky-950 opacity-70 font-medium '>NodeJs</p>
@@ -71,8 +74,8 @@ function App() {
       <section id='proj'>
       <h2 className='text-xl text-sky-900 font-semibold capitalize mb-4 m-16 '>Selected Projects</h2>
         
-        <div className='w-full flex flex-wrap px-16 gap-5' id="tools">
-          <div className="w-[45%] bg-white rounded-lg shadow-lg overflow-hidden">
+        <div className='w-full block sm:flex flex-wrap px-2 sm:px-16 space-y-4 sm:gap-5' id="tools">
+          <div className="sm:w-[45%] w-full bg-white rounded-lg shadow-lg overflow-hidden">
             <div className="img">
               <a href="https://newsletter-seven-beige.vercel.app">
                               <img src="./Frame 23 (1).svg" alt="" />
@@ -81,7 +84,7 @@ function App() {
             <h3 className="text-lg font-medium ml-6 mt-6 opacity-80">News Letter Page</h3>
             <p className='text-md font-semibold ml-6 mb-6 text-md capitalize flex gap-4'><span>HTML</span><span>CSS</span></p>
           </div>
-          <div className="w-[45%] bg-white rounded-lg shadow-lg overflow-hidden">
+          <div className="sm:w-[45%] w-full bg-white rounded-lg shadow-lg overflow-hidden">
             <div className="img">
               <a href="https://landin-two.vercel.app">
                               <img src="./Frame 24.svg" alt="" />
@@ -90,7 +93,7 @@ function App() {
             <h3 className="text-lg font-medium ml-6 mt-6 opacity-80">Room Landing Page</h3>
             <p className='text-md font-semibold ml-6 mb-6 text-md capitalize flex gap-4'><span>React</span><span>Typescript</span></p>
           </div>
-          <div className="w-[45%] bg-white rounded-lg shadow-lg overflow-hidden">
+          <div className="sm:w-[45%] w-full bg-white rounded-lg shadow-lg overflow-hidden">
             <div className="img">
                 <a href="https://shorten-url-tan.vercel.app">
                                 <img src="./Frame 25.svg" alt="" />
@@ -99,19 +102,19 @@ function App() {
             <h3 className="text-lg font-medium ml-6 mt-6 opacity-80">Shortly Landing Page</h3>
             <p className='text-md font-semibold ml-6 mb-6 text-md capitalize flex gap-4'><span>React</span><span>Tailwindcss</span></p>
           </div>
-          <div className="w-[45%] bg-white rounded-lg shadow-lg overflow-hidden">
+          <div className="sm:w-[45%] w-full bg-white rounded-lg shadow-lg overflow-hidden">
             <div className="img">
               <a href="https://intro-drop.vercel.app">
                               <img src="./Frame 26.svg" alt="" />
               </a>
             </div>
             <h3 className="text-lg font-medium ml-6 mt-6 opacity-80">Snap Landing Page</h3>
-            <p className='text-md font-semibold ml-6 mb-6 text-md capitalize flex gap-4'><span>React</span><span>Tailwindcssc</span></p>
+            <p className='text-md font-semibold ml-6 mb-6 text-md capitalize flex gap-4'><span>React</span><span>Tailwindcss</span></p>
           </div>
         </div>
       </section>
-      <section className='w-full flex p-16' id="hirem">
-        <div className="w-1/2 space-y-5 p-6">
+      <section className='w-full flex flex-col sm:flex-row sm:p-16 p-4' id="hirem">
+        <div className="sm:w-1/2 w-full space-y-5 p-6">
           <h2 className='text-4xl font-bold text-sky-900 '>
             Contact
           </h2>
@@ -121,7 +124,7 @@ function App() {
             as possible
           </p>
         </div>
-        <div className="w-1/2 p-6" >
+        <div className="sm:w-1/2 w-full p-6" >
           <form action="" className='space-y-6'>
             <div className="w-full h-6">
               <input type="text" required className='outline-none w-full h-full  invalid:border-red-400 focus:border-black border-2 border-sky-300 rounded-md p-4' placeholder='NAME' />
@@ -130,20 +133,25 @@ function App() {
               <input type="email" className='outline-none invalid:border-red-400 w-full h-full  focus:border-black border-2 border-sky-300 rounded-md p-4' placeholder="EMAIL" />
             </div>
             <div  className="w-full h-16">
-              <textarea required name="" id="" className='outline-none w-full h-full  focus:border-black border-2 border-sky-300 rounded-md p-4'  placeholder='MESSAGE'></textarea>
+              <input required name="" id="" value={salue} onChange={(e) => setSalue(e.target.value)} className='outline-none w-full h-full  focus:border-black border-2 border-sky-300 rounded-md p-4'  placeholder='MESSAGE'></input>
             </div>
-            <input className="px-5 py-2 rounded-md w-40 text-white cursor-pointer  invalid:border-red-400 bg-orange-500" type="submit" value="Shoot Email" />
+            
+
+              <a className='mt-4 block' href={`mailto:lemonyellow951@gmail.com?subject=${Subject}%20Here&body=${salue}`}>
+                          <input className="px-5 py-2 rounded-md w-40 text-white cursor-pointer  invalid:border-red-400 bg-orange-500" type="submit" value="Shoot Email" />
+
+              </a>
           </form>
         </div>
       </section>
-      <footer className='bg-orange-300 capitalize text-white font-semibold flex justify-between p-16 w-full'>
+      <footer className='bg-orange-300 capitalize text-white font-semibold flex justify-between p-6 sm:p-16 w-full'>
         <div className="right">
           <h2>
              &#169; Prostty 2024
           </h2>
         </div>
         <div className="flex gap-4">
-          <a>
+          <a href='https://github.com/00YellowLemon'>
             github
           </a>
           <a>
--- src/Nav/Nav.tsx
+++ src/Nav/Nav.tsx
@@ -1,12 +1,42 @@
-
+import { GrClose } from "react-icons/gr";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from "react";
 
 export default function Nav(){
+    const [open,setOpen] = useState(false);
+
+    function toggle(){
+        setOpen(!open)
+    }
+
+
     return(
-        <div className="w-full px-6 py-4 flex fixed opacity-100 bg-white shadow-xl justify-between items-center">
+        <div className="w-full px-6 py-4 flex relative opacity-100 bg-white shadow-xl justify-between items-center">
             <div className="h-7">
                 <img className="h-full w-auto rounded-md" src="Prostty.jpg" alt="" />
             </div>   
-            <ul className="flex items-center gap-6">
+            <ul className="sm:flex hidden items-center gap-6">
+                <li>
+                    <a href="#proj"  className="capitalize text-sky-950 opacity-60 hover:opacity-95 font-medium text-md">projects</a>
+                </li>
+                <li>
+                    <a href="#about me " className="capitalize text-sky-950 opacity-60 hover:opacity-95 font-medium text-md">about me</a>
+                </li>
+                <li>
+                    <a href="#tech"  className="capitalize text-sky-950 opacity-60 hover:opacity-95 font-medium text-md">tools</a>
+                </li>
+            </ul> 
+            <div className="px-5 hidden sm:block py-2 rounded-md bg-orange-500">
+                <a href="#hirem" className="font-semibold font-md text-white opacity-60 no-underline">Hire me</a>
+            </div>
+            {!open && <div className="sm:hidden block">
+                 <RxHamburgerMenu onClick={toggle}/>  
+            </div>}   
+            {open && <div className="p-6 absolute pt-8 top-3 gap-6 right-4 w-40 shadow-md rounded-lg bg-white">
+                <div className="absolute top-4 right-4">
+                     <GrClose onClick={toggle}/>
+                </div>
+                <ul className="flex text-left mb-6 flex-col items-center gap-6">
                 <li>
                     <a href="#proj"  className="capitalize text-sky-950 opacity-60 hover:opacity-95 font-medium text-md">projects</a>
                 </li>
@@ -19,7 +49,8 @@ export default function Nav(){
             </ul> 
             <div className="px-5 py-2 rounded-md bg-orange-500">
                 <a href="#hirem" className="font-semibold font-md text-white opacity-60 no-underline">Hire me</a>
-            </div>        
+            </div>
+            </div>  }   
         </div>
     )
 }
